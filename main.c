eaf2d40869def804df08c247aa7bce8c6f0f137e
Shafi Swapnil <swapnil.env@gmail.com>
2022-12-06 04:23:30 +0600

    switch statement (digit translation)

--- main.c
+++ main.c
@@ -4,13 +4,21 @@
 
 int main()
 {
-    int a;
-    scanf("%d", &a);
+    int input;
+    printf("input a number (one digit)\n");
+    scanf("%d", &input);
 
-    if (a%2==0) {
-        printf("%d is even", a);
-    } else {
-        printf("%d is odd", a);
+    switch (input) {
+        case 0: printf("Zero"); break;
+        case 1: printf("One"); break;
+        case 2: printf("Two"); break;
+        case 3: printf("Three"); break;
+        case 4: printf("Four"); break;
+        case 5: printf("Five"); break;
+        case 6: printf("Six"); break;
+        case 7: printf("Seven"); break;
+        case 8: printf("Eight"); break;
+        case 9: printf("Nine"); break;
     }
 
     return 0;
