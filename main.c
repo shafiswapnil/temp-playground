9051586f6a57a35bee4109bbb07a451238a7f432
Shafi Swapnil <swapnil.env@gmail.com>
2022-12-06 06:49:14 +0600

    more test case on determine if it is power of 2

--- main.c
+++ main.c
@@ -7,9 +7,18 @@ int main()
     int input;
     scanf("%d", &input);
 
-    if (ceil(log2(input)) == floor(log2(input))) {
-        printf("Yes");
-    } else {
+    if (input > 0) {
+        if (ceil(log2(input)) == floor(log2(input))) {
+            printf("Yes");
+        }
+    }
+    else if (input == 0) {
+        printf("Zero is not a valid input");
+    }
+    else if (input < 0) {
+        printf("Negative is not valid input");
+    }
+    else {
         printf("No");
     }
 
