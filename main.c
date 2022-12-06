ddc5b1fab01fd3a74d640fba3cfb0a9de8705fcb
Shafi Swapnil <swapnil.env@gmail.com>
2022-12-06 06:36:10 +0600

    determine if it is power of 2

--- main.c
+++ main.c
@@ -5,20 +5,12 @@
 int main()
 {
     int input;
-    printf("input a number (one digit)\n");
     scanf("%d", &input);
 
-    switch (input) {
-        case 0: printf("Zero"); break;
-        case 1: printf("One"); break;
-        case 2: printf("Two"); break;
-        case 3: printf("Three"); break;
-        case 4: printf("Four"); break;
-        case 5: printf("Five"); break;
-        case 6: printf("Six"); break;
-        case 7: printf("Seven"); break;
-        case 8: printf("Eight"); break;
-        case 9: printf("Nine"); break;
+    if (ceil(log2(input)) == floor(log2(input))) {
+        printf("Yes");
+    } else {
+        printf("No");
     }
 
     return 0;
